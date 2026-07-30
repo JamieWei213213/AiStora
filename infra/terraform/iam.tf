657d084ec9328896a4401aa409a057eb11353908
JamieWei213213 <jamiejwei@gmail.com>
2026-07-30 13:03:01 -0700

    Tighten ECS deploy role resource scope

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -149,7 +149,9 @@ data "aws_iam_policy_document" "github_deploy" {
       "ecs:DescribeServices",
       "ecs:UpdateService",
     ]
-    resources = [aws_ecs_service.app.id]
+    resources = [
+      "arn:aws:ecs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/${aws_ecs_cluster.main.name}/${aws_ecs_service.app.name}",
+    ]
   }
 
   statement {
